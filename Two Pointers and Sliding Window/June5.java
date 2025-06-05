bcf4779ea2585581cd7fab532313a143bc499410
theagegupta <ayushgupta11580@gmail.com>
2025-06-05 23:59:57 +0530

    Count all Subarrays that have a,b,c

---
+++ Two Pointers and Sliding Window/June5.java
@@ -0,0 +1,32 @@
+class June5 {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        String str = "aaabbbcbabc";
+        System.out.println(sol.countSubstring(str));
+    }
+}
+
+class Solution {
+    public static int countSubstring(String s) {
+        // code here
+
+        int lastSeen[] = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            lastSeen[i] = -1;
+        }
+
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            lastSeen[s.charAt(i) - 'a'] = i;
+
+            if (lastSeen[0] != -1 && lastSeen[1] != -1 && lastSeen[2] != -1) {
+                // all 3 characters are present
+                count += 1 + Math.min(Math.min(lastSeen[0], lastSeen[1]), lastSeen[2]);
+            }
+        }
+
+        return count;
+    }
+}
