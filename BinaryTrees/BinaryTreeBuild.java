65eaca45554ce5baed18e7829472fafbf4be2f5c
theagegupta <ayushgupta11580@gmail.com>
2025-07-10 22:54:18 +0530

    Learned Trees

---
+++ BinaryTrees/BinaryTreeBuild.java
@@ -0,0 +1,79 @@
+// package BinaryTrees;
+
+class BinaryTreeBuild {
+
+    static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+            this.left = null;
+            this.right = null;
+        }
+    }
+
+    static class BinaryTree {
+        int idx = -1; // to track the index of the nodes array
+
+        public Node buildTree(int nodes[]) {
+            idx++;
+
+            if (nodes[idx] == -1) {
+                return null;
+            }
+
+            Node newNode = new Node(nodes[idx]);
+            newNode.left = buildTree(nodes);
+            newNode.right = buildTree(nodes);
+
+            return newNode;
+        }
+
+        public void preOrder(Node root) {
+            if (root == null) {
+                System.out.print(-1 + " ");
+                return;
+            }
+
+            System.out.print(root.data + " ");
+            preOrder(root.left);
+            preOrder(root.right);
+        }
+
+        public void inOrder(Node root) {
+            if (root == null) {
+                // System.out.print(-1 + " ");
+                return;
+            }
+            inOrder(root.left);
+            System.out.print(root.data + " ");
+            inOrder(root.right);
+        }
+
+        public void postOrder(Node root) {
+            if (root == null) {
+                return;
+            }
+
+            postOrder(root.left);
+            postOrder(root.right);
+            System.out.print(root.data + " ");
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int nodes[] = { 1, 2, 4, -1, -1, 5, -1, -1, 3, -1, 6, -1, -1 };
+        BinaryTree bt = new BinaryTree();
+        Node root = bt.buildTree(nodes);
+
+        // System.out.println(root.data);
+        bt.preOrder(root);
+        System.out.println();
+        bt.inOrder(root);
+        System.out.println();
+        bt.postOrder(root);
+    }
+}
